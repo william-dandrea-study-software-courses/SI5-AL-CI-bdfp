d77ddcfa9e4ee485eadc0bb2dd4ef8a8151e3dcf
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-09-24 10:54:11 +0200

    Implement the structure of the BFF with the health module / dockerfiles (production and development version) and update dependencies

--- bff/src/app.module.ts
+++ bff/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import appConfig from "./shared/config/app.config";
+import swaggeruiConfig from "./shared/config/swaggerui.config";
+import {HealthModule} from "./health/health.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, swaggeruiConfig],
+    }),
+      HealthModule,
+  ],
+  providers: [],
 })
 export class AppModule {}
