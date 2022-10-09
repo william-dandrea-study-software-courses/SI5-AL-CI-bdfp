1d3b79c7d95d3ac365206700244dcd5b7c29209d
Yann-Brault <brlt.yann@gmail.com>
2022-10-09 11:57:50 +0200

    change route

--- client/src/services/TableService.js
+++ client/src/services/TableService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const tableUrl = "http://localhost:5301/tables";
+const tableUrl = "http://localhost:5301/table-resume/infos";
 
 const getAllTables = () => axios.get(tableUrl);
 
