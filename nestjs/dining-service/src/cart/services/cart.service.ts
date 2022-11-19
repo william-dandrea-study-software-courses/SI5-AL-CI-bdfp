5f57d6539727a7e62e5be0c63c8c05eac101b330
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-11-19 18:08:59 +0100

    Correct create user-cart

--- nestjs/dining-service/src/cart/services/cart.service.ts
+++ nestjs/dining-service/src/cart/services/cart.service.ts
@@ -1,4 +1,4 @@
-import {HttpStatus, Injectable} from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import {ItemInCart, TableCart, TableCartDocument, UserCart} from "../schemas/table-cart.schema";
 import {MenuItemDto} from "../dto/menu-item.dto";
 import {Model} from "mongoose";
@@ -65,7 +65,7 @@ export class CartService {
         return this.tableCartModel.findOneAndDelete({table_number: tableNumber}, { returnDocument: 'after' });
     }
 
-    public async createUserCart(tableNumber: number): Promise<TableCart> {
+    public async createUserCart(tableNumber: number): Promise<UserCart> {
         const currentCart: TableCart = await this.tableCartModel.findOne({table_number: tableNumber});
 
         if (currentCart != null) {
@@ -75,9 +75,14 @@ export class CartService {
             userCart.items_in_cart = [];
             userCart.price = 0;
 
-            await this.tableCartModel.findOneAndUpdate({table_number: tableNumber}, {'$push': {'user_carts': userCart}})
+            const result = await this.tableCartModel.findOneAndUpdate({table_number: tableNumber}, {'$push': {'user_carts': userCart}})
 
-            return await this.tableCartModel.findOne({table_number: tableNumber});
+            if (result) {
+                return userCart;
+            } else {
+                throw new HttpException("Cannot write in DB", HttpStatus.UNPROCESSABLE_ENTITY)
+            }
+            // return await this.tableCartModel.findOne({table_number: tableNumber});
         }
 
         throw new NoGlobalCartExistException(tableNumber)
